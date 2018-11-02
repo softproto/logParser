d5a97b9575b894130ef19f1a56f172bbcb9931ba
yedmit <softproto@gmail.com>
2018-11-02 11:08:00 +0300

    final ver. console out. no DB

--- main.go
+++ main.go
@@ -28,7 +28,6 @@ const fileCheckTimeout = 1000
 //main
 func main() {
 	logfilesList := os.Args[1:]
-	fmt.Println("\nStarted")
 
 	if len(logfilesList) == 0 {
 		logfilesList = append(logfilesList, "data3.dat", "data2.dat", "data1.dat")
@@ -36,19 +35,24 @@ func main() {
 		//log.Fatal("wrong usage")
 	}
 
+	fmt.Println("\nStarted for:", logfilesList)
+
 	dataChannel := make(chan logRecord, dataChannelBuffer)
 
 	for filenameIndex := range logfilesList {
 		fmt.Println("run parser for", logfilesList[filenameIndex])
 		go readLinesFromFile(dataChannel, logfilesList[filenameIndex], fileCheckTimeout)
 	}
+	fmt.Println("all files processed")
 
 	for {
 		for currentLogRecord := range dataChannel {
-			fmt.Println("<-", currentLogRecord)
+			fmt.Println("\n<-New log record")
+			fmt.Println("log_time:", currentLogRecord.log_time)
+			fmt.Println("log_message:", currentLogRecord.log_message)
+			fmt.Println("file_name:", currentLogRecord.file_name)
+			fmt.Println("log_format:", currentLogRecord.log_format)
 		}
 	}
-
-	fmt.Scanln()
 
 }
